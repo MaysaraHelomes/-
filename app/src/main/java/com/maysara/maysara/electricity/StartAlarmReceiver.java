b9f7bf0be9ee2f26b31fb21d989b3e0880eb6fbe
Maysara <maysaradeveloper@gmail.com>
2016-12-29 20:05:59 +0200

    update1

---
+++ app/src/main/java/com/maysara/maysara/electricity/StartAlarmReceiver.java
@@ -0,0 +1,31 @@
+package com.maysara.maysara.electricity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+/**
+ * Created by maysara on 12/27/16.
+ */
+
+public class StartAlarmReceiver extends BroadcastReceiver {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor ;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        sharedPreferences = context.getSharedPreferences("alarmstate",Context.MODE_PRIVATE);
+
+        if (getState())
+            context.startService( new Intent(context, AlarmService.class));
+
+    }
+
+    private  boolean getState()
+    {
+        return sharedPreferences.getBoolean("state",false);
+    }
+
+
+}
